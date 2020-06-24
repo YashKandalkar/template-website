da2b25fdc9fd057d540c84985adf73821324c605
Yash Kandalkar <yashsk.yk@gmail.com>
2020-06-24 20:07:21 +0530

    Updates

---
+++ precache-manifest.b9d5f349d6dd9efbe68b274d43e48514.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae8e85ef1f26e949ee12afc2ad5dfa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7356a9809dc76babd544",
+    "url": "/static/css/main.afafa9f3.chunk.css"
+  },
+  {
+    "revision": "6f75b360c3848a723c85",
+    "url": "/static/js/2.ffe978d9.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.ffe978d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7356a9809dc76babd544",
+    "url": "/static/js/main.3bb304ae.chunk.js"
+  },
+  {
+    "revision": "4a58f07abc3c608b88a0",
+    "url": "/static/js/runtime-main.118059e6.js"
+  },
+  {
+    "revision": "de8edcc1e39f2bbd43d085709cc9e2c7",
+    "url": "/static/media/canary.de8edcc1.png"
+  }
+]);
